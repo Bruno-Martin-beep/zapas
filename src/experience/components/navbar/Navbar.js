ae93329b6d380cf382cb92d8f4ddfb2efb32276b
Bruno-Martin-beep <brunico040@gmail.com>
2022-04-27 23:11:46 -0300

    add back

--- src/experience/components/navbar/Navbar.js
+++ src/experience/components/navbar/Navbar.js
@@ -27,16 +27,17 @@ const Navbar = ({ currentModel, handleDone, openCheckout }) => {
         zapaz
       </h2>
       <h2 className="navbar beta">beta</h2>
-      <svg
+      <div
         className={classNames("navbar back", {
           visible: currentModel.editing,
         })}
         onClick={HandleBack}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 48 48"
       >
-        <path d="M28.05 36 16 23.95 28.05 11.9 30.2 14.05 20.3 23.95 30.2 33.85Z" />
-      </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+          <path d="M28.05 36 16 23.95 28.05 11.9 30.2 14.05 20.3 23.95 30.2 33.85Z" />
+        </svg>
+        back
+      </div>
       <Background />
       <h2 className="navbar bag-info" onClick={open}>
         bag {bag.length}
