1a94bddf7c18e6d999ed9f1cbf037574bbbb6777
Bruno-Martin-beep <brunico040@gmail.com>
2022-05-05 20:09:58 -0300

    Add remove info

--- src/experience/components/panel/Panel.js
+++ src/experience/components/panel/Panel.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./panel.scss";
 import classNames from "classnames";
 import { useDispatch, useSelector } from "react-redux";
-import { editShoe, changePrevMesh, selectCurrentShoe } from "../../features/modelsListSlice";
+import {
+  editShoe,
+  changePrevMesh,
+  selectCurrentShoe,
+} from "../../features/modelsListSlice";
 import MeshSelector from "./MeshSelector";
 import ColorPickerPanel from "./ColorPickerPanel";
 import ColorsSaved from "../ColorsSaved";
 import AddToBag from "./AddToBag";
+import ColorsInfo from "./ColorsInfo";
 
 const Panel = ({ handleDone }) => {
   const dispatch = useDispatch();
@@ -27,6 +32,7 @@ const Panel = ({ handleDone }) => {
       <MeshSelector currentModel={currentModel} />
       <ColorPickerPanel currentModel={currentModel} handleColor={handleColor} />
       <div className="colors-saved-panel">
+        <ColorsInfo />
         <ColorsSaved
           classParent={"selectors"}
           classChild={"select-color"}
