5b2b85535e6325564993df7b7b34948b65f3e6c5
Bruno-Martin-beep <brunico040@gmail.com>
2022-05-05 23:31:24 -0300

    Add checkout

--- src/experience/components/Checkout.js
+++ src/experience/components/Checkout.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { useClosing } from "../hooks/useClosing";
 import classNames from "classnames";
+import { useClosing } from "../hooks/useClosing";
 import "./checkout.scss";
 import Modal from "./Modal";
 
-const Checkout = ({setOpen}) => {
+const Checkout = ({ setOpen }) => {
   const [showCheckout, closing, close] = useClosing(setOpen);
 
   if (!showCheckout) return <></>;
   return (
-     <Modal
+    <Modal
       className={classNames("checkout-cont", { closing: closing })}
       close={close}
     >
-    </Modal> 
+      <h2 className="checkout-text-1">Hey, this is a proyect for prove my skills 👨‍💻</h2>
+      <h2 className="checkout-text-2">For more info about me, here is <a target="_blank" rel="noreferrer" href="https://bruno-martin-beep.github.io/portfolio/">my portfolio</a></h2>
+      
+    </Modal>
   );
 };
 
